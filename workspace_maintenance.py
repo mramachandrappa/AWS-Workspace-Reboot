90e52843387531ae2f3d0cc15dfb80e22de58fda
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-05-02 20:53:28 +0530

    Used paginators to find unhealthy workspace by fetching all workspaceIds

--- workspace_maintenance.py
+++ workspace_maintenance.py
@@ -5,11 +5,11 @@ import time
 
 
 class Workspaces:
-#====================== Role based connection to AWS resource ===============================
+    # ====================== Role based connection to AWS resource ===============================
     def __init__(self):
         sts_client = boto3.client('sts')
         assumed_role_object = sts_client.assume_role(
-            RoleArn="<ROLE-ARN>",                       #AWS role arn
+            RoleArn="<ROLE_ARN>",
             RoleSessionName="AssumeRoleSession1"
         )
 
@@ -21,90 +21,77 @@ class Workspaces:
             aws_session_token=credentials['SessionToken'],
         )
 
-#====================== Fetches list of Workspace ID's whose status is set to UNHEALTHY =========
+        self.paginator = self.connect.get_paginator('describe_workspaces')
+
+    # ====================== Fetches list of Workspace ID's whose status is set to UNHEALTHY =========
     def get_workspace(self, directory_id):
         workspace_id = []
         status = []
-        response = None
-        token = None
-        while True:
-            try:
-                if token is None:
-                    response = self.connect.describe_workspaces(
-                        DirectoryId=directory_id
-                    )
-                elif token is not None:
-                    response = self.connect.describe_workspaces(
-                        DirectoryId=directory_id,
-                        NextToken=token
-                    )
 
-                token = response["NextToken"]
+        page_response = self.paginator.paginate(
+            DirectoryId=directory_id,
+            PaginationConfig={
+                'MaxItems': 300,
+                'PageSize': 25
+            }
+        )
 
-                for work_id in response["Workspaces"]:
-                    for key, value in work_id.items():
-                        if key == "WorkspaceId":
-                            workspace_id.append(value)
-                        if key == "State":
-                            status.append(value)
-            except KeyError:
-                print("Loop is completed")
-                break
-            except Exception as e:
-                return e
+        for response in page_response:
+            for work_id in response["Workspaces"]:
+                for key, value in work_id.items():
+                    if key == "WorkspaceId":
+                        workspace_id.append(value)
+                    if key == "State":
+                        status.append(value)
 
         unhealthy_workspaces = []
         if len(workspace_id) == len(status):
-           for tups in zip(workspace_id, status):
+            for tups in zip(workspace_id, status):
                 for id in tups:
                     if id.__contains__("UNHEALTHY"):
                         unhealthy_workspaces.append(tups[0])
 
         return unhealthy_workspaces
 
-#====================== Reboots UNHEALTHY workspaces returned from get_workspace()================
-    def reboot_workspace(self, workspaces):
-        if workspaces:
-            for id in workspaces:
-                self.connect.reboot_workspaces(RebootWorkspaceRequests=[
-                    {
+    # ====================== Reboots UNHEALTHY workspaces returned from get_workspace()================
+    def reboot_workspace(self, unhealthy_workspaces):
+        for id in unhealthy_workspaces:
+            self.connect.reboot_workspaces(RebootWorkspaceRequests=[
+                {
                     'WorkspaceId': id
-                            },
-                        ]
-                    )
+                },
+            ]
+            )
 
-            time.sleep(30)
+        time.sleep(30)
 
-            status=[]
-            i = 0
-            while True:
-                response = self.connect.describe_workspaces(
-                    WorkspaceIds=workspaces
-                )
+        status = []
+        i = 1
+        while i in range(0,5):
+            page_response = self.paginator.paginate(
+                WorkspaceIds=unhealthy_workspaces
+            )
 
-                if i > 8:
-                    return False
-                else:
-                    if len(workspaces) != len(status):
-                        for work_id in response["Workspaces"]:
-                            for key, value in work_id.items():
-                                if key == "State":
-                                    print("Current Status {}".format(str(value)))
-                                    if value == "AVAILABLE":
-                                        status.append(value)
-                                    else:
-                                        time.sleep(30)
-                                        i = i + 1
-                    elif len(workspaces) == len(status):
-                        break
-            return True
-        else:
-            return False
+            for response in page_response:
+                for work_id in response["Workspaces"]:
+                    for key, value in work_id.items():
+                        if key == "State":
+                            print("status", value)
+                            status.append(value)
 
-#====================== Sends a Status message to Hangout Chat Room ==============================
+            if len(unhealthy_workspaces) == status.count("AVAILABLE"):
+                return True
+            else:
+                status = []
+                i = i + 1
+                time.sleep(30)
+
+        return False
+
+    # ====================== Sends a Status message to Hangout Chat Room ==============================
     def athens_bot(self, message, url):
         bot_message = {
-                    "text": message
+            "text": message
         }
 
         message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
@@ -112,10 +99,10 @@ class Workspaces:
         http_obj = Http()
 
         response = http_obj.request(
-                uri=url,
-                method='POST',
-                headers=message_headers,
-                body=json.dumps(bot_message),
+            uri=url,
+            method='POST',
+            headers=message_headers,
+            body=json.dumps(bot_message),
         )
 
         return response
@@ -123,28 +110,36 @@ class Workspaces:
 
 def main():
     obj = Workspaces()
-    directory_id = "<DIRECTORY_id>" #Workspaces DirectoryID
+    directory_id = "<DIRECTORY-ID>"
 
-    workspaces = obj.get_workspace(directory_id)
+    unhealthy_workspaces = obj.get_workspace(directory_id)
 
-    if workspaces:
+    if unhealthy_workspaces:
         message = "*Athens Workspace Notification:*\n\n" \
                   "```Status      : UNHEALTHY\n" \
-                  "WorkspaceID : " + str(workspaces) + "```\n" \
-                  "*_Rebooting Workspaces..._*\n"
+                  "WorkspaceIDs : " + str(unhealthy_workspaces) + "```\n" \
+                                                                 "*_Rebooting Workspaces..._*\n"
 
         sucess_mes = "*Athens Workspace Notification:*\n\n" \
-                     "*_ Unhealthy Workspaces are Successfully Rebooted!_*" \
+                     "*_UNHEALTHY Workspaces are Successfully Rebooted!_*" \
                      "```Status      : AVAILABLE\n" \
-                     "WorkspaceID : " + str(workspaces) + "```\n"
+                     "WorkspaceIDs : " + str(unhealthy_workspaces) + "```\n"
 
-        url = "<Webhook URL>"           # Hangout's Chat room webhook URL
+        fail_mes = "*Athens Workspace Notification:*\n\n" \
+                   "*_Some of UNHEALTHY workspaces are not coming to AVAILABLE state. Please check!_*" \
+                   "```WorkspaceIDs : " + str(unhealthy_workspaces) + "```\n"
+
+        url = "<HANGOUT_WEBHOOK_URL>"
+        
         obj.athens_bot(message, url)
 
-        if obj.reboot_workspace(workspaces) is True:
+        if obj.reboot_workspace(unhealthy_workspaces) is True:
             obj.athens_bot(sucess_mes, url)
+        else:
+            obj.athens_bot(fail_mes, url)
     else:
         print("No UNHEALTHY Workspaces found!")
+
 
 if __name__ == "__main__":
     main()
